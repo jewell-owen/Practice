16cad79d8610d03d37a8f64d3d09087536476e39
Owen Jewell <ojewell@iastate.edu>
2024-02-09 14:30:43 -0600

    added new JUnit tests, add text files for notes

---
+++ ChessGame/src/Test/CheckmateTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheckmateTest {
+	
+	//Implement tests for CheckMate testing
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
